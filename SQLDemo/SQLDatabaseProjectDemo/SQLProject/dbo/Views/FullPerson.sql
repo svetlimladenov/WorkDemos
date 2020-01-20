59f0f3623db07d06f921de97cddc6bb85a963465
Svetlin Mladenov <svetlimladenov@gmail.com>
2020-01-20 16:27:09 +0200

    Added polymer demo and sql db project demo

--- SQLDemo/SQLDatabaseProjectDemo/SQLProject/dbo/Views/FullPerson.sql
+++ SQLDemo/SQLDatabaseProjectDemo/SQLProject/dbo/Views/FullPerson.sql
@@ -1,6 +1,6 @@
 ﻿CREATE VIEW [dbo].[FullPerson]
 	AS
-	SELECT [p].[Id] AS PersonId, [p].[FirstName], [p].[LastName], 
+	SELECT [p].[FirstName], [p].[LastName], 
 		   [a].[Id] AS AddressId, [a].[StreetAddress], [a].[City], [a].[State], [a].[ZipCode]
 	FROM dbo.Person p
 	LEFT JOIN dbo.Address a ON p.Id = a.PersonId
