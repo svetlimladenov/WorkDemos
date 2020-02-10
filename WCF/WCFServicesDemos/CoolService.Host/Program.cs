5c950a036286c1406a88621ba3c485c2eea108aa
Svetlin Mladenov <svetlimladenov@gmail.com>
2020-02-10 21:20:04 +0200

    Adding old wcf demos

---
+++ WCF/WCFServicesDemos/CoolService.Host/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoolService.Host
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(typeof(CoolService)))
+            {
+                host.Open();
+                Console.WriteLine("Started working"));
+                Console.Read();
+            }
+        }
+    }
+}
---
+++ WCF/WCFServicesDemos/CoolService/CoolService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace CoolService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "CoolService" in both code and config file together.
+    public class CoolService : ICoolService
+    {
+        public string DoWork()
+        {
+            return "Work"!;
+        }
+
+        public string GetCredits(DateTime dueDate)
+        {
+            return "credit credit credit";
+        }
+    }
+}
