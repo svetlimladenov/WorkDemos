b9fe2d679e8a809148098cf14818337c93a0629c
Svetlin <svetlimladenov@gmail.com>
2020-02-14 10:37:16 +0200

    Added ShoppingItem db table.

---
+++ WebApiForTests/DTOs/WebApi.DTO/ShoppingCart/ShoppingItemDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebApi.DTO.ShoppingCart
+{
+    public class ShoppingItemDTO
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Manufacturer { get; set; }
+    }
+}
--- WebApiForTests/Database/WebApi.Data/Migrations/20200212081704_ShoppingCartAdd.Designer.cs
+++ WebApiForTests/Database/WebApi.Data/Migrations/20200212081704_ShoppingCartAdd.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebApi.Data;
 
 namespace WebApi.Data.Migrations
 {
     [DbContext(typeof(WebApiDbContext))]
-    partial class WebApiDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200212081704_ShoppingCartAdd")]
+    partial class ShoppingCartAdd
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
