b62af342c04a88efc3fa0f643533e88789a267e0
Svetlin Mladenov <svetlimladenov@gmail.com>
2020-02-15 02:05:05 +0200

    fixed conficts

--- WebApiForTests/Tests/WebApi.Tests/ProductServicesTests1.cs
+++ WebApiForTests/Tests/WebApi.Tests/ProductServicesTests1.cs
@@ -19,7 +19,6 @@ namespace WebApi.Tests
         {
             mockDbContext = new Mock<WebApiDbContext>();
             services = new ProductServices(mockDbContext.Object);
-<<<<<<< HEAD
             var mockedProducts = new Mock<DbSet<Product>>();
             mockedProducts.Object.Add(new Product()
             {
@@ -30,20 +29,12 @@ namespace WebApi.Tests
             });
 
             mockDbContext.Setup(x => x.Products).Returns(mockedProducts.Object);
-=======
-            
->>>>>>> 327bf2c23f81a81621b5777037cb4a01bdc8c225
         }
 
         [Fact]
         public void GetAllProducts_ShouldReturnAllActiveProducts()
         {
-<<<<<<< HEAD
-            var products = this.services.GetAllProducts();
-            Assert.Equal("Credit", products.FirstOrDefault()?.Name);
-=======
            
->>>>>>> 327bf2c23f81a81621b5777037cb4a01bdc8c225
         }
     }
 }
